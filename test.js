fae5c4427433a13c2892a929d10600dc886109f9
Gregor <gregor@martynus.net>
2017-07-07 17:39:38 -0700

    feat: multiple tests with same chrome instance

--- test.js
+++ test.js
@@ -1,17 +1,38 @@
 const HeadlessChrome = require('simple-headless-chrome')
 const test = require('tap').test
 
-const browser = new HeadlessChrome({
-  headless: true
-})
-
-test('example.com', async function (t) {
+test('Headless Chrome', async function (group) {
+  const browser = new HeadlessChrome({
+    headless: false
+  })
   await browser.init()
-  await browser.goTo('https://example.com')
-  const result = await browser.evaluate(() => document.title)
-  await browser.close(true)
 
-  t.equal(result.result.value, 'Example Domain', 'Page title is "Example Domain"')
+  group.test('example.com', async function (t) {
+    // see available actions https://git.io/vQKpy
+    await browser.goTo('https://example.com')
+    const result = await browser.evaluate(() => document.title)
 
-  t.end()
+    t.equal(result.result.value, 'Example Domain', 'Page title is "Example Domain"')
+
+    t.end()
+  })
+
+  group.test('Notes PWA', async function (t) {
+    // see available actions https://git.io/vQKpy
+    await browser.goTo('https://sii.im/playground/notes/')
+    await browser.type('form input', 'Remember the milk\r')
+    const result = await browser.evaluate(() => document.querySelector('ul').textContent.trim())
+
+    t.equal(result.result.value, 'Remember the milk', 'Adds "Remember the milk" to list')
+
+    // TODO: figure out how to simulate Chrome being "offline"
+
+    t.end()
+  })
+
+  group.tearDown(async () => {
+    // true = kill with SIGHUP
+    // https://git.io/vQKpP
+    await browser.close(true)
+  })
 })
